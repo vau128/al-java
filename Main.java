39d3c4f898e4d52bef3b41cb9b8ad99044280930
test <leenny0912@gmail.com>
2025-03-19 13:18:17 +0900

    test commit

--- Main.java
+++ Main.java
@@ -1,15 +1,24 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int index =0;
+        int[] arr = new int[N];
+        while(st.hasMoreTokens()){
+            arr[index] = Integer.parseInt(st.nextToken());
+            index++;
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[0] + " " + arr[N-1]);
 
     }
 }
