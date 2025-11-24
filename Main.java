c74cbfdb7e0f0bae75b10f5f1899184063c1e292
test <leenny0912@gmail.com>
2025-11-24 20:26:15 +0900

    test

--- Main.java
+++ Main.java
@@ -1,24 +1,16 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        String N = sc.nextLine();
+        int B = sc.nextInt();
 
-        int index =0;
-        int[] arr = new int[N];
-        while(st.hasMoreTokens()){
-            arr[index] = Integer.parseInt(st.nextToken());
-            index++;
-        }
-        Arrays.sort(arr);
-        System.out.println(arr[0] + " " + arr[N-1]);
+        System.out.println("dd");
+
+        
+
 
     }
 }
